54f138443c540cf8d77ce3078a66143415ef24bf
Vinay-Daripelly <daripellyvinay@gmail.com>
2024-10-08 20:21:29 +0530

    first commit

---
+++ client/src/Data/data.js
@@ -0,0 +1,36 @@
+export const userMenu=[
+    {
+        name:"Home",
+        path:"/",
+        icon:"fa-solid fa-house",
+    },
+    {
+        name:"Appointment",
+        path:"/appointments",
+        icon:"fa-solid fa-list"
+    },
+    {
+        name:"Apply Trainer",
+        path:"/apply-trainer",
+        icon:"fa-solid fa-dumbbell"
+    },
+];
+
+//admin menu
+export const adminMenu=[
+    {
+        name:"Home",
+        path:"/",
+        icon:"fa-solid fa-house",
+    },
+    {
+        name:"Trainers",
+        path:"/admin/trainers",
+        icon:"fa-solid fa-dumbbell"
+    },
+    {
+        name:"Users",
+        path:"/admin/users",
+        icon:"fa-solid fa-user"
+    },
+];
